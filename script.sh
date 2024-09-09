e97a3d44c1700fb7f32387f12797c5c256cd7d3c
alan01777 <camp.alan3@gmail.com>
2024-09-09 10:53:31 -0300

    Delete unnecessary files and update .gitignore

---
+++ script.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Check if the virtual environment already exists
+if [ ! -d ".venv" ]
+then
+    python3 -m venv .venv
+fi
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Check if activation was successful
+if [ $? -ne 0 ]
+then
+    echo "Failed to activate virtual environment"
+    exit 1
+fi
+
+# Install the required packages
+pip install -r requirements.txt
+
+# Check if installation was successful
+if [ $? -ne 0 ]
+then
+    echo "Failed to install required packages"
+    exit 1
+fi
+
+# Navigate to the script directory
+cd src
+
+# Check if navigation was successful
+if [ $? -ne 0 ]
+then
+    echo "Failed to navigate to script directory"
+    exit 1
+fi
+
+# Run the script, passed as an argument
+python3 main.py
+
+# Check if script ran successfully
+if [ $? -ne 0 ]
+then
+    echo "Failed to run script"
+    exit 1
+fi
+
+# Deactivate the environment
+deactivate
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from datetime import datetime
 from whatsapp_notifier import WhatsAppNotifier
 
 if __name__ == "__main__":
@@ -8,30 +9,44 @@ if __name__ == "__main__":
 
     logging.basicConfig(filename='Logs/logs.log', level=logging.INFO)
 
-    # You should be able to modify the lines below, using the source files you made
     tax_data_sources = {
-        "DAS": {
-            "source": "Data/DAS.xlsx",
-            "day": "20"
-        },
-        "FGTS": {
-            "source": "Data/FGTS.xlsx",
-            "day": "05"
-        },
-        "PIS": {
-            "source": "Data/PIS.xlsx",
-            "day": "10"
-        },
-        "COFINS": {
-            "source": "Data/COFINS.xlsx",
-            "day": "15"
-        },
-        "PARCELAMENTO": {
-            "source": "Data/PARCELAMENTO.xlsx",
-            "day": "20"
+            "DAS": {
+                "source": "Data/DAS.xlsx",
+                "day": "20"
+            },
+            # "FGTS": {
+            #     "source": "Data/FGTS.xlsx",
+            #     "day": "05"
+            # },
+            # "PIS": {
+            #     "source": "Data/PIS.xlsx",
+            #     "day": "10"
+            # },
+            # "COFINS": {
+            #     "source": "Data/COFINS.xlsx",
+            #     "day": "15"
+            # },
+            # "PARCELAMENTO": {
+            #     "source": "Data/PARCELAMENTO.xlsx",
+            #     "day": "20"
+            # }
         }
-    }
 
     for tax, info in tax_data_sources.items():
-        notifier = WhatsAppNotifier(tax, info["source"])
-        notifier.send_messages(info["day"])
+        # Get the current day
+        current_day = datetime.now().day
+
+        # Gets the day the tax needs to be paid and substracts a number
+        tax_day = int(info["day"]) - 2
+
+        # Check if the tax day falls on a weekend
+        tax_date = datetime.now().replace(day=tax_day)
+        if tax_date.weekday() > 4:  # 5 and 6 corresponds to Saturday and Sunday
+            tax_day -= tax_date.weekday() - 4  # adjust to the preceding Friday
+
+        # If the current day is the same as the tax day, send the messages
+        if current_day == tax_day:
+            notifier = WhatsAppNotifier(tax, info["source"])
+            notifier.send_messages(info["day"])
+        else:
+            print('Not today..')
